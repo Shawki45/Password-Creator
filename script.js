d0fb8ba5a21bd3669988ec2dc01e750e8b1bad76
Shawki45 <shawkihaifa@yahoo.com>
2023-12-21 19:31:50 -0500

    added the alert and prompt

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  window.alert("Click Ok To Continue")
+  window.prompt("How many characters you want it to be?")
+  window.confirm("Are you sure?") 
 
   passwordText.value = password;
+  function generatePassword() {
+    var length = 10,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
 
 }
 
